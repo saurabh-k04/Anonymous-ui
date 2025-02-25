329f32fd13dbddb2a83bc445ef4121138d93122c
Saurabh <saurabhkhandelwal548@gmail.com>
2025-02-25 22:48:03 +0530

    Production changes

--- src/app/service/data/posts-data.service.ts
+++ src/app/service/data/posts-data.service.ts
@@ -1,33 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from 'src/app/posts/posts.component';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsDataService {
 
+  private apiUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) { }
 
   retrieveAllPosts(username: any){
-    return this.http.get<Post[]>(`http://localhost:8080/users/${username}/posts`);
+    return this.http.get<Post[]>(this.apiUrl + `/users/${username}/posts`);
     //console.log("Hello world Bean service")
   }
 
   retrievePost(username: any, id: any){
     console.log("check");
-    return this.http.get<Post>(`http://localhost:8080/users/${username}/posts/${id}`);
+    return this.http.get<Post>(this.apiUrl + `/users/${username}/posts/${id}`);
     //console.log("Hello world Bean service")
   }
 
   updatePost(username: any, id: any, post: any){
-    return this.http.put(`http://localhost:8080/users/${username}/posts/${id}`, post);
+    return this.http.put(this.apiUrl + `/users/${username}/posts/${id}`, post);
     //console.log("Hello world Bean service")
   }
 
   createPost(username: any, post: any){
     console.log("in");
-    return this.http.post(`http://localhost:8080/users/${username}/posts`, post);
+    return this.http.post(this.apiUrl + `/users/${username}/posts`, post);
     //console.log("Hello world Bean service")
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://anonymousme-backend.onrender.com' // Render backend URL
+};
+  
