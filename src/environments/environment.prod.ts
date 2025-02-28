01d58ffd3cf0c627a0a02b93d699211dd9cdd663
saurabh-k04 <68682170+saurabh-k04@users.noreply.github.com>
2025-02-28 19:22:43 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8080' // Render backend URL
+    apiUrl: 'https://anonymousmechat-backend.onrender.com' // Render backend URL
 };
   
